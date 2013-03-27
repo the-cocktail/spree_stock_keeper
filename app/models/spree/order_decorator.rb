48e939b37fe06f6a01946c308cd226527461a404
nando <fernando.garcia@the-cocktail.com>
2013-03-27 15:48:25 +0100

    prevent double decorator loading...

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,21 +1,21 @@
-Spree::Order.class_eval do
-  state_machine.before_transition :to => :payment, :do => :stock_keep_payment_minutes!
-  state_machine.before_transition :to => :complete, :do => :expire_stock_keepings!
-
-  protected
-
-  def expire_stock_keepings!
-    stock_keeper_expires_at! Time.now
-  end
+unless Spree::Order.instance_methods.include? :expire_stock_keepings!
+  Spree::Order.class_eval do
+    state_machine.before_transition :to => :payment, :do => :stock_keep_payment_minutes!
+    state_machine.before_transition :to => :complete, :do => :expire_stock_keepings!
   
-  def stock_keep_payment_minutes!
-    stock_keeper_expires_at! 10.minutes.from_now
-  end
-
-  private
-
-  def stock_keeper_expires_at!(timestamp)
-    update_column :stock_keeper_expires_at, timestamp
-    line_items.each {|li| li.update_column :stock_keeper_expires_at, timestamp}
+    def expire_stock_keepings!
+      stock_keeper_expires_at! Time.now
+    end
+    
+    def stock_keep_payment_minutes!
+      stock_keeper_expires_at! 10.minutes.from_now
+    end
+  
+    private
+  
+    def stock_keeper_expires_at!(timestamp)
+      update_column :stock_keeper_expires_at, timestamp
+      line_items.each {|li| li.update_column :stock_keeper_expires_at, timestamp}
+    end
   end
 end
