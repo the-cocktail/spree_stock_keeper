93d87c581fb8704e2d60a9ded82a89d9d2799b07
nando <fernando.garcia@the-cocktail.com>
2013-05-21 14:22:57 +0200

    sufficient_stock? yes cause is already ours

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -9,6 +9,15 @@ Spree::LineItem.class_eval do
     with_stock_keeped_for(variant).sum(:quantity)
   end
 
+  def sufficient_stock?
+    return true if Spree::Config[:allow_backorders]
+    if new_record?
+      variant.on_hand >= quantity
+    else
+      variant.on_hand >= (quantity - (self.changed_attributes['quantity'] || quantity).to_i)
+    end
+  end
+
   private
     def set_stock_keeper_expires_at
       if self.stock_keeper_expires_at.blank? or self.order_id_changed?
