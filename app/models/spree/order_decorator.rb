302a5f91ba6ca72952a7793b7707658eeb73185a
nando <fernando.garcia@the-cocktail.com>
2013-09-03 12:52:32 +0200

    default order expiration
    sometimes you create an order initially without line_items

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,5 +1,6 @@
 unless Spree::Order.instance_methods.include? :expire_stock_keepings!
   Spree::Order.class_eval do
+    before_create :set_stock_keeper_expires_at
     state_machine.before_transition :to => :payment, :do => :stock_keep_payment_minutes!
     state_machine.before_transition :to => :complete, :do => :expire_stock_keepings!
   
@@ -22,12 +23,20 @@ unless Spree::Order.instance_methods.include? :expire_stock_keepings!
     def stock_keepings_expires_in_seconds
       stock_keeper_expires_at && (stock_keeper_expires_at - Time.now).to_i
     end
+
+    def set_stock_keeper_expiration!
+      stock_keeper_expires_at! Spree::StockKeeper.expires_at
+    end
   
     private
   
     def stock_keeper_expires_at!(timestamp)
       update_column :stock_keeper_expires_at, timestamp
       line_items.each {|li| li.update_column :stock_keeper_expires_at, timestamp}
+    end
+
+    def set_stock_keeper_expires_at
+      stock_keeper_expires_at = Spree::StockKeeper.expires_at 
     end
   end
 end
